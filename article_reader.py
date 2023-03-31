a37a075fb32eb5ed88ab1c5c1758c7ce4c7d779f
DR <86169204+LearnFL@users.noreply.github.com>
2023-03-30 20:49:38 -0400

    Update article_reader.py

--- article_reader.py
+++ article_reader.py
@@ -1,24 +1,50 @@
-from PyPDF2 import PdfReader
-from pathlib import Path
 import re
 import os
+from pathlib import Path
 from threading import Thread, Lock
+import csv
+from PyPDF2 import PdfReader
+from openpyxl import Workbook, load_workbook
+import pandas as pd
 
-dir_path = Path(r"YOUR FOLDER PATH")
+class CountMissing:
+    def __init__(self):
+        self.count_missing = 0
+        self.count = 0
+        self.count_hits = 0
+        self.aList = []
 
+    def missing(self, file):
+        self.count_missing += 1
+        self.aList.append(file)
+    
+    def count_pdf(self):
+        self.count += 1
+
+    def count_found(self):
+        self.count_hits += 1
+
+    def report(self):
+        return f'Total read: {self.count}, Found: {self.count_hits}, total could not read: {self.count_missing}, files could not read by name: {self.aList}'
+    
+pdf_report= CountMissing()
 
 class InputClass:
     def __init__(self, pdf_file):
         self.pdf_file = pdf_file
         self.lock = Lock()
+        self.failed = []
 
     def read(self):
         with self.lock:
-            with open(self.pdf_file, 'rb') as f:
-                pdf = PdfReader(f)
-                pages = pdf.pages
-                for idx, page in enumerate(pages, 1):
-                    yield (len(pages), idx, self.pdf_file, page)
+            try: 
+                with open(self.pdf_file, 'rb') as f:
+                    pdf = PdfReader(f)
+                    pages = pdf.pages
+                    for idx, page in enumerate(pages, 1):
+                        yield (len(pages), idx, self.pdf_file, page)
+            except:
+                yield None
 
     @classmethod
     def generate_inputs(cls, data_dir):
@@ -28,68 +54,149 @@ class InputClass:
                 yield cls(os.path.join(data_dir, file))
 
 
+        
 class PdfSearch:
-    def __init__(self, input, pattern, GrabExtra):
+    def __init__(self, input, pattern, to_txt, to_csv, to_excel, grab_extra, params, print_on_screen):
         self.input = input
         self.report = []
         self.lock = Lock()
         self.pattern = pattern
-        self.GrabExtra = GrabExtra
-
+        self.grab_extra = grab_extra
+        self.to_txt = to_txt
+        self.to_csv = to_csv
+        self.to_excel = to_excel
+        self.params = params
+        self.print_on_screen = print_on_screen
 
     def dump_txt(self):
         with self.lock:
-            with open(os.path.join(dir_path, 'report.txt'), "a") as writer:
-                for entry in self.report:
-                    writer.write(f'{entry} \n\n')
+            if self.to_txt:
+                with open(self.to_txt, "a") as writer:
+                    for entry in self.report:
+                        writer.write(f"{entry[0]}\n{entry[1]}\n{entry[2]}\n\n")
+    
+    def dump_csv(self):
+        with self.lock:
+            if self.to_csv:
+                with open(self.to_csv, "w", encoding='UTF8') as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    for entry in self.report:
+                        csv_writer.writerow([entry[0]])
+                        csv_writer.writerow([entry[1]])
+                        csv_writer.writerow([entry[2]])
 
-    def present_data(self, pattern, GrabExtra=False):
+    def dump_excel(self):
+        if self.to_excel:
+            df = pd.DataFrame(self.report, columns=['File', 'Page', 'Text'])
+            with self.lock:
+                try:
+                    if not df.empty:
+                        data_file = pd.read_excel(self.to_excel, engine='openpyxl')
+                        data_file = pd.concat([df, data_file])
+                        data_file.to_excel(self.to_excel)
+                except:
+                    if not df.empty:
+                        df.to_excel(self.to_excel)
+
+    def present_data(self, pattern):    
+        if self.input.read() == None or None in self.input.read():
+            pdf_report.missing(self.input.pdf_file)
+            return
+        
+        with self.lock:
+            pdf_report.count_pdf()  
+
         for pagesN, idx, file_name, page in self.input.read():
             result = pattern.finditer(page.extract_text())
+
             for i in result:
-                if GrabExtra == False:
-                    print(f'File Name: {file_name}, Page: {idx} out of {pagesN}, {i.group()}')
-                    self.report.append(f'File Name: {file_name}, Page: {idx} out of {pagesN}, {i.group()}')
-
-                else:
+                if self.grab_extra == 0:
+                    self.report.append([f'File Name: {file_name}', f'Page: {idx} of {pagesN}', f'{i.group()}'])
+                    if self.print_on_screen:
+                        print(f'File Name: {file_name}, Page: {idx} of {pagesN}, {i.group()}')
+                    
+                if self.grab_extra > 0:
                     try:
-                        print(f'File Name: {file_name}, Page: {idx} out of {pagesN}, {page.extract_text()[i.start()-50 : i.end()+50]}')
+                        self.report.append([f'File Name: {file_name}', f'Page: {idx} of {pagesN}', f'{page.extract_text()[i.start()-self.grab_extra : i.end()+self.grab_extra]}'])
+                        if self.print_on_screen:
+                            print(f'File Name: {file_name}, Page: {idx} of {pagesN}, {page.extract_text()[i.start()-self.grab_extra : i.end()+self.grab_extra]}')
                     except:
-                        print(f'File Name: {file_name}, Page: {idx} out of {pagesN}, {i.group()}')
-                        self.report.append(f'File Name: {file_name}, Page: {idx} out of {pagesN}, {i.group()}')
-                    else:
-                        self.report.append(f'File Name: {file_name}, Page: {idx} out of {pagesN}, {page.extract_text()[i.start()-50 : i.end()+50]}')
+                        self.report.append([f'File Name: {file_name}', f'Page: {idx} of {pagesN}', f'{i.group()}'])
+                        if self.print_on_screen:
+                            print(f'File Name: {file_name}, Page: {idx} of {pagesN}, {i.group()}')
+                pdf_report.count_found()
 
-        self.dump_txt()
+            if self.to_csv: self.dump_csv()
+            if self.to_txt: self.dump_txt()
+            if self.to_excel: self.dump_excel()
 
+    def build_patterns(self):
+        include = ''.join([f"(?=.*\\b{i}\\b.*)" for i in params.get('include', '')])
+        exclude = ''.join([f"(?!.*\\b{i}\\b.*)" for i in params.get('exclude', '')]) 
+        keywords = '|'.join([f'\\b{i}\\b' for i in params.get('keywords', '')])
+        pattern = r""+include+exclude+".*("+keywords+").*"    
+        return pattern
+    
     def filter(self):
-        pattern = re.compile(self.pattern, re.IGNORECASE)
-        self.present_data(pattern, GrabExtra=self.GrabExtra)
-
+        if self.pattern != None:
+            pattern = re.compile(self.pattern, re.IGNORECASE)
+            self.present_data(pattern)
+        elif self.params != None:
+            pattern = re.compile(self.build_patterns(), re.IGNORECASE)
+            self.present_data(pattern)
+            
     @classmethod
-    def build_workers(cls, input_class, data_dir, pattern, GrabExtra=False):
+    def build_workers(cls, data_dir, input_class, pattern, grab_extra, to_txt, to_csv, to_excel, params, print_on_screen):
         workers = []
         for input_data in input_class.generate_inputs(data_dir):
-            workers.append(cls(input_data, pattern, GrabExtra))
+            workers.append(cls(input_data, pattern, to_txt, to_csv, to_excel, grab_extra, params, print_on_screen))
         return workers
 
 
+
 def execute(workers):
+    print('Reading files, it may take a minute...')
+    
     threads = [Thread(target=w.filter) for w in workers]    
     for thread in threads: thread.start()
     for thread in threads: thread.join()
+      
+    print(pdf_report.report())
 
 
-def search_pdf(*, worker_class, input_class, data_dir, pattern, GrabExtra):
-    workers = worker_class.build_workers(input_class,data_dir, pattern, GrabExtra)
+ 
+def search_pdf(data_dir, *, params=None, pattern=None, to_txt=None, to_csv=None, to_excel=None, worker_class=PdfSearch, input_class=InputClass, grab_extra=0, print_on_screen=True):
+
+    if not os.path.exists(data_dir):
+        raise IOError(f"{data_dir}: Invalid input destination. Select a diferent destination.")
+    
+    if to_txt is not None:
+        head, tail = os.path.split(to_txt)
+        if Path(tail).suffix != '.txt':
+            to_txt = os.path.join(to_txt, "report.txt")
+
+    if to_csv is not None:
+        head, tail = os.path.split(to_csv)
+        if Path(tail).suffix != '.csv':
+            to_csv = os.path.join(to_csv, "report.csv")
+
+    if to_excel is not None:
+        head, tail = os.path.split(to_excel)
+        suff = Path(tail).suffix
+        if suff != '.xlsx':
+            to_excel = os.path.join(to_excel, "report.xlsx")
+   
+    if not isinstance(grab_extra, int) or grab_extra < 0:
+        raise ValueError('grab_extra: must provide integer greater than 0.')
+
+    workers = worker_class.build_workers(data_dir, input_class, pattern, grab_extra, to_txt, to_csv, to_excel, params, print_on_screen)
     return execute(workers)
 
 
-main_subject = 'mobility'
-key_words = ['hydrogen', 'AlOH', 'H', 'Ca', 'Mg', 'Li', 'alkali', 'metals', 'charge','compensator', 'hole', 'electron', 'OH', 'travel', 'move', 
-             'sweep', 'sweeping', 'axis', 'current', 'ion', 'covalent', 'valent', 'Al'
-            ]
-
-pattern = r".*"+f"\\b{main_subject}\\b"+"\s*\w*\s*("+'|'.join([f'\\b{i}\\b' for i in key_words])+").*"
-
-search_pdf(worker_class=PdfSearch, input_class=InputClass, data_dir=dir_path, pattern=pattern, GrabExtra=True)
+'''
+EXAMPLE:
+dir_path = Path(r'SOME PATH')
+save_dir = os.path.join(dir_path, 'my_report.txt')
+pattern = r'\w{1}'
+search_pdf(dir_path, to_txt=save_dir, pattern=pattern, grab_extra=10)
+'''
